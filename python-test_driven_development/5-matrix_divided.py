94e2e7d53dc193ffd81bf06ab4086b5e9f4f0da5
Samuel Bereket <samuelbereket123@gmail.com>
2026-08-22 18:48:04 +0300

    2

---
+++ python-test_driven_development/5-matrix_divided.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Module that contains the function matrix_divided.
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    Divides all elements of a matrix by div, rounded to 2 decimal places.
+    """
+    msg = "matrix must be a matrix (list of lists) of integers/floats"
+
+    if not isinstance(matrix, list) or len(matrix) == 0:
+        raise TypeError(msg)
+
+    for row in matrix:
+        if not isinstance(row, list):
+            raise TypeError(msg)
+        for elem in row:
+            if type(elem) not in (int, float):
+                raise TypeError(msg)
+
+    row_len = len(matrix[0])
+    for row in matrix:
+        if len(row) != row_len:
+            raise TypeError("Each row of the matrix must have the same size")
+
+    if type(div) not in (int, float):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    return [[round(elem / div, 2) for elem in row] for row in matrix]
