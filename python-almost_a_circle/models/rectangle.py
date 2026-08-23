0f91649be883d84b762c12f431f29849efb6ad61
Samuel Bereket <samuelbereket123@gmail.com>
2026-08-23 20:43:14 +0300

    c

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,7 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """Represent a rectangle with setter validation and printing logic."""
+    """Represent a rectangle with setter validation, area, display, and str."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize a new Rectangle.
@@ -82,3 +82,9 @@ class Rectangle(Base):
         """Print the Rectangle instance using the '#' character."""
         for _ in range(self.height):
             print("#" * self.width)
+
+    def __str__(self):
+        """Return the print() and str() representation of the Rectangle."""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.x, self.y, self.width, self.height
+        )
