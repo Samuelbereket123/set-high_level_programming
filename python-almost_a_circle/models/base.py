7fe91bb96effb9072b02b96f9baa03bc76e33887
Samuel Bereket <samuelbereket123@gmail.com>
2026-08-23 21:27:04 +0300

    sdf

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -45,3 +45,19 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Write the JSON string representation of list_objs to a file.
+
+        Args:
+            list_objs (list): A list of instances inheriting from Base.
+        """
+        filename = "{}.json".format(cls.__name__)
+        dict_list = []
+        if list_objs is not None:
+            dict_list = [obj.to_dictionary() for obj in list_objs]
+        json_str = cls.to_json_string(dict_list)
+        with open(filename, "w", encoding="utf-8") as f:
+            f.write(json_str)
